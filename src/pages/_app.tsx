704d7089459ff045aae2eb8cdb609d88e75a54e4
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-09 16:49:27 -0600

    add entries provider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,21 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { darkTheme, lightTheme } from "@/themes";
 import { UIProvider } from "@/context/ui";
+import { EntriesProvider } from "@/context/entries";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UIProvider>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </UIProvider>
+    <EntriesProvider>
+      <UIProvider>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UIProvider>
+    </EntriesProvider>
   );
 }
