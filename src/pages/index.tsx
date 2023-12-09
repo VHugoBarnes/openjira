c6f2e6730954563d15499defb85a234a921f4e31
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-12-09 16:10:30 -0600

    Design of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,33 @@
 import { MainLayout } from "@/layouts";
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 
 export default function Home() {
   return (
-    <MainLayout>
-      <Typography>
-        Hola Mundo
-      </Typography>
+    <MainLayout title="Home">
+      <Grid container spacing={2} padding={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Pending" />
+
+            <CardContent>
+              {/* Add new entry */}
+              {/* List entries */}
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="In Progress" />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Done" />
+          </Card>
+        </Grid>
+      </Grid>
     </MainLayout>
   );
 }
